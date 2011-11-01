63194b6ae4a1e22507be004109f844ddba6ce615
Laurens Van Houtven <_@lvh.cc>
2011-11-01 11:38:31 +0100

    Fixed call syntax

--- typewriter.sh
+++ typewriter.sh
@@ -2,28 +2,6 @@
 
 set -x
 
-build() {
-	$TEMPDIR = `mktemp -d -t typewriter`
-	if [ $? -ne 0 ]; then
-    	echo "$0: can't create tempdir!"
-    	exit 1
-	fi
-	echo "$0: building in $TEMPDIR..."
-	
-	git clone $1 $TEMPDIR
-
-	#pandoc README* --5sS -o $TEMPDIR/index.html
-	cat README* | $TEMPDIR/build > $TEMPDIR/index.html
-	if [ $? -ne 0 ]; then
-		echo "$0: build failed!"
-		exit 1
-	fi
-
-	touch $TEMPDIR/.typewriter
-
-	echo "$0: build finished"
-}
-	
 prepare() {
 	git update-index --ignore-submodules --refresh &> /dev/null
 	if [ $? -ne 0 ]; then
@@ -56,18 +34,40 @@ newBranch() {
 	git clean -fdx
 }
 
+build() {
+	$TEMPDIR = `mktemp -d -t typewriter`
+	if [ $? -ne 0 ]; then
+    	echo "$0: can't create tempdir!"
+    	exit 1
+	fi
+	echo "$0: building in $TEMPDIR..."
+	
+	git clone $1 $TEMPDIR
+
+	#pandoc README* --5sS -o $TEMPDIR/index.html
+	cat README* | $TEMPDIR/build > $TEMPDIR/index.html
+	if [ $? -ne 0 ]; then
+		echo "$0: build failed!"
+		exit 1
+	fi
+
+	touch $TEMPDIR/.typewriter
+
+	echo "$0: build finished"
+}
+
 commit () {
 	TARGET="$PWD/"
 	cd $TEMPDIR
 	git checkout-index -a --prefix=$TARGET
 	cd $TARGET
 
-	git add -A *
+	git add -A
 	git commit -m "typewriter build `date -u +%Y-%m-%dT%H:%MZ`"
 	#git push origin gh-pages
 	#git checkout master
 }
 
-build()
-prepare()
-commit()
+build
+prepare
+commit
