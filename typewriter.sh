b14d16e539b101bdcb76a09fe9ef60a6366db028
Laurens Van Houtven <_@lvh.cc>
2011-11-01 11:48:48 +0100

    Made variable names a bit more meaningful

--- typewriter.sh
+++ typewriter.sh
@@ -3,69 +3,69 @@
 set -x
 
 prepare() {
-	git update-index --ignore-submodules --refresh &> /dev/null
-	if [ $? -ne 0 ]; then
-		echo "$0: git repository not clean!"
-		exit 1
-	fi
+    git update-index --ignore-submodules --refresh &> /dev/null
+    if [ $? -ne 0 ]; then
+        echo "$0: git repository not clean!"
+        exit 1
+    fi
 
-	git pull origin gh-pages &> /dev/null
-	if [$? -eq 0]; then
-		existingBranch
-	else
-		newBranch
-	fi
+    git pull origin gh-pages &> /dev/null
+    if [$? -eq 0]; then
+        existingBranch
+    else
+        newBranch
+    fi
 
-	echo "$0: gh-pages branch prepared"
+    echo "$0: gh-pages branch prepared"
 }
 
 existingBranch() {
-	git checkout gh-pages &> /dev/null
-	if [! -f .typewriter]; then
-		echo "$0: gh-pages branch exists, but was not built by typewriter!"
-		exit 1
-	fi
-	git ls-files -z | xargs -0 rm -f
+    git checkout gh-pages &> /dev/null
+    if [! -f .typewriter]; then
+        echo "$0: gh-pages branch exists, but was not built by typewriter!"
+        exit 1
+    fi
+    git ls-files -z | xargs -0 rm -f
 }
 
 newBranch() {
-	git symbolic-ref HEAD refs/heads/gh-pages
-	rm .git/index
-	git clean -fdx
+    git symbolic-ref HEAD refs/heads/gh-pages
+    rm .git/index
+    git clean -fdx
 }
 
 build() {
-	$TEMPDIR = `mktemp -d -t typewriter`
-	if [ $? -ne 0 ]; then
-    	echo "$0: can't create tempdir!"
-    	exit 1
-	fi
-	echo "$0: building in $TEMPDIR..."
-	
-	git clone $1 $TEMPDIR
+    BUILDDIR=$(mktemp -d -t typewriter)
+    if [ $? -ne 0 ]; then
+        echo "$0: can't create temporary build directory!"
+        exit 1
+    fi
+    echo "$0: building in $BUILDDIR..."
+    
+    git clone $1 $BUILDDIR
 
-	#pandoc README* --5sS -o $TEMPDIR/index.html
-	cat README* | $TEMPDIR/build > $TEMPDIR/index.html
-	if [ $? -ne 0 ]; then
-		echo "$0: build failed!"
-		exit 1
-	fi
+    #pandoc README* --5sS -o $BUILDDIR/index.html
+    cat README* | $BUILDDIR/build > $BUILDDIR/index.html
+    if [ $? -ne 0 ]; then
+        echo "$0: build failed!"
+        exit 1
+    fi
 
-	touch $TEMPDIR/.typewriter
+    touch $BUILDDIR/.typewriter
 
-	echo "$0: build finished"
+    echo "$0: build finished"
 }
 
 commit () {
-	TARGET="$PWD/"
-	cd $TEMPDIR
-	git checkout-index -a --prefix=$TARGET
-	cd $TARGET
+    TARGET="$PWD/"
+    cd $BUILDDIR
+    git checkout-index -a --prefix=$TARGET
+    cd $TARGET
 
-	git add -A
-	git commit -m "typewriter build `date -u +%Y-%m-%dT%H:%MZ`"
-	#git push origin gh-pages
-	#git checkout master
+    git add -A
+    git commit -m "typewriter build $(date -u +%Y-%m-%dT%H:%MZ)"
+    #git push origin gh-pages
+    #git checkout master
 }
 
 build
